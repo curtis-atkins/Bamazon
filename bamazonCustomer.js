8fb40875b20117c71bbc73cb7816e5a98e90897b
catkins <catkinsjr28@gmail.com>
2017-08-10 20:10:30 -0400

    submission

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,7 +17,7 @@ var connection = mysql.createConnection({
 //otherwise the thread ID is logged and a query is run on the database
 connection.connect(function(err) {
     if (err) throw err;
-	
+    
 //function call for items in mysql table names products
     getBamazonItems();
 });
@@ -30,7 +30,7 @@ getBamazonItems = function() {
                 console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price);
             }
         });
-	
+    
 //function call for items selected by user
         pickItemNQuantity();
     }
@@ -38,11 +38,11 @@ getBamazonItems = function() {
 //declaring a function that asks user what item and what quantity the want to purchase 
 //and returns it to the command line
 function pickItemNQuantity() {
-	
+    
 // query the database for all items available for purchase
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
-		
+        
 //prompt the user for which item they'd like to purchase
         inquirer.prompt([{
                 name: "item",
@@ -50,23 +50,49 @@ function pickItemNQuantity() {
                 choices: function() {
                     var productsArray = [];
                     for (var i = 0; i < res.length; i++) {
-                        productsArray.push(res[i].item_id + " - " + res[i].product_name);
+                        productsArray.push(res[i].item_id + " "); // + " - " + res[i].product_name);
                     }
                     return productsArray;
                 },
-                message: "Which item would you like to purchase?"
+                message: "Use the keyboard to press the number of the item above you would like to purchase"
             },
-						 
+                         
 //prompt the user for quantity of the item they'd like to purchase
             {
                 name: "quantity",
                 type: "input",
                 message: "Type the quantity of this item you would like to purchase"
             }
-        ]).then(function(answers) {
-			
+
+
+
+//******* I could not get this portion of the project to work.  There seems to be something wrong with how 
+//******* chosenItem variable to where it prints undefined to the console.
+//******* also when I use chosenItem.stock_quantity OR res[i].stock_quantity, an error is thrown
+//******* any advice on that portion would be greatly appreciated!
+        ]).then(function(answer) {
+            var chosenItem;
+            for (var i = 0; i < res.length; i++) {
+                if (res[i].item_id === answer.item) {
+                    chosenItem = res[i];
+                }
+                console.log(res[i]);
+                console.log(chosenItem);
+            }
+            if (chosenItem.stock_quantity < answer.quantity) {
+                connection.query("UPDATE products SET ? WHERE ?", [{
+                    product_name: chosenItem.item
+                }, {
+                    stock_quantity: chosenItem.quantity - answer.quantity
+                }], function(error) {
+                    if (error) throw err;
+                    
 // get the information of the chosen item and quantity and display it on the command line
-            console.log("You have purchased " + answers.quantity + answers.item.split("-").pop());
-        })
-    })
+                    console.log("You have purchased " + answer.quantity + answer.item.split("-").pop());
+                });
+            } else {
+                console.log("Insufficient quantity!");
+            }
+        });
+    });
 }
